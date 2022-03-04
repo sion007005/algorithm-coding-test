957c9afc9c8d95ad0a43fe63147d53e0ecb49c36
Sion <sion007005@gmail.com>
2022-03-04 23:31:06 +0900

    Initial commit

---
+++ src/main/java/sion/study/algorithmcodingtest/AlgorithmCodingTestApplication.java
@@ -0,0 +1,13 @@
+package sion.study.algorithmcodingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmCodingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmCodingTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sion/study/algorithmcodingtest/AlgorithmCodingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package sion.study.algorithmcodingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmCodingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
